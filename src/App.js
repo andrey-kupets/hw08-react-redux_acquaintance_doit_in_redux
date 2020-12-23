209f91f5bc88ebef0151855414a64f82f0db27d3
andrey-kupets <andrey.kupets@i.ua>
2020-12-24 02:47:34 +0300

    add start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, {useEffect} from "react";
 import './App.css';
+import {useSelector, useDispatch} from "react-redux";
 
-function App() {
+export default function App() {
+  const counter = useSelector(({counter}) => counter + 1);
+    console.log(counter)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`)
+        .then((response) => response.json())
+        .then((json) => {
+          dispatch({ type: "SET_TODO", payload: json });
+        });
+  }, [counter]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={() => counter}>inc</button>
+        <button onClick={() => dispatch({type: 'CHANGE_TODO_STATUS'})}>change todo status</button>
+        <button onClick={() => dispatch({type: "CHANGE_TODO_TITLE", payload: Math.random() })}>change todo title</button>
+        <h1>Counter value: {counter}</h1>
+            <>
+                {/*<h2>{state.todo.id}</h2>*/}
+                {/*<h2>{state.todo.title}</h2>*/}
+                {/*<h2>{state.todo.completed.toString()}</h2>*/}
+            </>
     </div>
   );
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_TODO': {
+            return action.payload;
+        }
+        case 'CHANGE_TODO_STATUS': {
+            return {
+                ...state, todo: {...state.todo, completed: !state.completed}
+            };
+        }
+        case 'CHANGE_TODO_TITLE': {
+            return {
+                ...state, todo: {...state.todo, title: action.payload}
+            };
+        }
+        default: {
+            return state;
+        }
+    }
+};
+
+
+const initialState = {
+    counter: 1,
+    todo: {
+        userId: null,
+        id: null,
+        title: '',
+        completed: false
+    }
+};
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
